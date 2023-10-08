7f44c981c40a6e86929572c1152521e6236a058c
greg <gregory.stepanov@live.ru>
2023-10-08 20:21:51 +0300

    upd 404 page style

--- src/components/notfound-page/notfound-page.jsx
+++ src/components/notfound-page/notfound-page.jsx
@@ -1,9 +1,15 @@
+import "./notfound-page.scss"
+import {Link} from "react-router-dom";
+
 const NotfoundPage = () => {
-    return (
-        <div>
-            404
-        </div>
-    )
+  return (
+    <div className="lostPage">
+      <h1>404</h1>
+      <p>
+        It looks like there is no such page,<br/> go back to the <Link to="/">main page</Link>.
+      </p>
+    </div>
+  )
 }
 
 export default NotfoundPage
